9747c6d7bf37d857c74820d9115c85baf966cd3a
saadWindows <saadiqbal1921@gmail.com>
2025-03-12 23:11:01 +0530

    v3

--- app/agentView.py
+++ app/agentView.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Hotel, Order
+from django.db.models import Sum
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def agenthome(request):
-    print('hiii')
-    return render(request, 'agent/agent.html')
+    if request.user.role != 'agent':
+        return HttpResponse('You are not allowed to view this page.............. ! Simon Go Back')
+    hotels = Hotel.objects.all()
+    total = Order.objects.aggregate(Sum('total'))['total__sum'] or 0
+    print(total)
+    return render(request, 'agent/agent.html',{'hotels':hotels, 'total':total})
