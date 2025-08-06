c39210b4a9f2c3ae7dc0fb71cfb3669e6af34f84
saadWindows <saadiqbal1921@gmail.com>
2025-08-07 00:08:20 +0530

    order collapse feature added

--- app/models.py
+++ app/models.py
@@ -55,7 +55,7 @@ class MenuItem(models.Model):
     category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-
+    active = models.BooleanField(default=True)
     def __str__(self):
         return self.name
 
