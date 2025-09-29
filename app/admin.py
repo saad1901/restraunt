c321798aa12741389462a2aee5de99ebe9bf7633
saad@Ubuntu <saadiqbal1921@gmail.com>
2025-09-29 16:38:04 +0530

    paymentRecords

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+
 admin.site.register(Table)
 admin.site.register(MenuCategory)
 admin.site.register(MenuItem)
@@ -8,4 +8,5 @@ admin.site.register(Order)
 admin.site.register(OrderItems)
 admin.site.register(Hotel)
 admin.site.register(User)
+admin.site.register(PaymentRecord)
 
